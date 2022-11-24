04f702e8c5071a0a8dab466e4c47817f73248c78
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-11-24 14:08:07 +0300

    database_helper works

---
+++ db/database_helper.dart
@@ -0,0 +1,62 @@
+
+import 'dart:async';
+import 'dart:core';
+
+// import 'package:autorization/flutter_app.dart';
+import 'package:autorization/src/user.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+
+class DatabaseHelper {
+  //empty constructor
+  DatabaseHelper();
+
+  Future openDatabase() async {
+    return await Hive.openBox<Map>('users');
+  }  
+
+  Future openBox() async {
+    return await Hive.box<Map>('users');
+  }
+
+  Future addUser({required Box box, required User user}) async {
+    Map userMap = user.toMap();
+    box.put('${user.email}', userMap);
+  }
+
+  Future userExists({required Box box, required User user}) async {
+    Map userMap = user.toMap();
+    if (box.get('${user.email}') != null) return '${user.password}';
+    return "no such user"; 
+  }
+}
+
+// void main() async {
+//   //starting and opening the database
+//   await Hive.initFlutter();
+
+//   var databaseHelper = await DatabaseHelper();
+
+//   var opendb = await databaseHelper.openDatabase();
+//   var openBox = await databaseHelper.openBox();
+
+//   //test data
+
+//   var user1 = User(email: 'user1@gmail.com', password: 'password1');
+//   var user2 = User(email: 'user2@gmail.com', password: 'password2');
+
+//   //unit tests (almost)
+
+//   var addedUser = await databaseHelper.addUser(box: openBox, user: user1);
+//   print('added user: $user1');
+
+//   print('------getting users:');
+//   print('------user exists, password:');
+//   var existingUser = await databaseHelper.userExists(box: openBox, user: user1);
+//   print(existingUser);
+//   print('------user doesn\'t exist:');
+//   var nonExistingUser = await databaseHelper.userExists(box: openBox, user: user2);
+//   print(nonExistingUser);
+
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,18 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+<<<<<<< Updated upstream
+=======
+import path_provider_macos
+import sqflite
+>>>>>>> Stashed changes
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+<<<<<<< Updated upstream
+=======
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+>>>>>>> Stashed changes
 }
