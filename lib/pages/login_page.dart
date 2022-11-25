8f0d04666ef1aae9ac08c0ea3420aa9b4718ee31
bukhanovskayamarina22 <116250199+bukhanovskayamarina22@users.noreply.github.com>
2022-11-25 22:33:21 +0300

    alignment

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -52,175 +52,174 @@ class MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) => MultiProvider(
-          providers: [
-            ChangeNotifierProvider(create: (context) => UsernameProvider()),
-            ChangeNotifierProvider(create: (context) => PasswordProvider()),
-          ],
-          builder: (context, child) {
-            return Center(
-              child: Form(
-                key: _formKey,
-                child: SizedBox(
-                  width: 350.0,
-                  height: 600.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: IconButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const SettingsPage()));
-                            },
-                            icon: const Icon(CustomIcons.settings)),
+    providers: [
+      ChangeNotifierProvider(create: (context) => UsernameProvider()),
+      ChangeNotifierProvider(create: (context) => PasswordProvider()),
+    ],
+    builder: (context, child) {
+      return Center(
+        child: Form(
+          key: _formKey,
+          child: SizedBox(
+            width: 350.0,
+            height: 600.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const SettingsPage()));
+                      },
+                      icon: const Icon(CustomIcons.settings)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).iconTheme.color,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            labelStyle: TextStyle(
-                              color: Theme.of(context).iconTheme.color,
-                            ),
-                            border: OutlineInputBorder(),
-                            label: Text(AppLocalizations.of(context)!
-                                .pageLoginUsername),
-                            hintText: AppLocalizations.of(context)!
-                                .pageLoginUsernameHelp,
-                          ),
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                    .hasMatch(value)) {
-                              return AppLocalizations.of(context)!
-                                  .pageLoginUsernameError;
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: TextFormField(
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            labelStyle: TextStyle(
-                              color: Theme.of(context).iconTheme.color,
-                            ),
-                            border: const OutlineInputBorder(),
-                            label: Text(AppLocalizations.of(context)!
-                                .pageLoginPassword),
-                            hintText: AppLocalizations.of(context)!
-                                .pageLoginPasswordHelp,
-                          ),
-                          // The validator receives the text that the user has entered.
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            if (value.length < 7) {
-                              return AppLocalizations.of(context)!
-                                  .pageLoginPasswordError;
-                            }
-                          },
-                        ),
-                      ),
-
+                      border: OutlineInputBorder(),
+                      label: Text(AppLocalizations.of(context)!
+                          .pageLoginUsername),
+                      hintText: AppLocalizations.of(context)!
+                          .pageLoginUsernameHelp,
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 70),
-                    child: Container(
-                      width: double.infinity,
-                      color: Colors.transparent,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          signIn(context);
-                        },
-                        child: Text(
-                            AppLocalizations.of(context)!.pageLoginButtonGoogle),
-
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: SizedBox(
-                          width: 350,
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              var databaseHelper = await DatabaseHelper();
-                              var opendb = await databaseHelper.openUserBox();
-                              var openBox = await databaseHelper.getUserBox();
-                              print(_formKey.currentState);
-                              // Validate returns true if the form is valid, or false otherwise.
-                              if (_formKey.currentState!.validate()) {
-                                var user = await User(
-                                    email: emailController.text,
-                                    password: passwordController.text);
-                                var userExistsString = await databaseHelper
-                                    .userExists(box: openBox, user: user);
-                                if (userExistsString != "no such user") {
-                                  // ignore: use_build_context_synchronously
-                                  Navigator.of(context).pushNamed(HomePage.tag);
-                                } else {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) =>
-                                        BuildPopupDialog(),
-                                  );
-                                  // Navigator.of(context).pushNamed(NoSuchUser.tag);
-                                }
-                                print(userExistsString);
-                                // If the form is valid, display a snackbar. In the real world,
-                                // you'd often call a server or save the information in a database.
-                                // ignore: use_build_context_synchronously
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text('Processing Data')),
-                                );
-                              }
-                            },
-                            child: const Text('Submit'),
-                          ),
-                        ),
-
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 70),
-                        child: Container(
-                          width: double.infinity,
-                          color: Colors.transparent,
-                          child: OutlinedButton(
-                            onPressed: () {
-                              signIn(context);
-                            },
-                            child: Text(AppLocalizations.of(context)!
-                                .pageLoginButtonGoogle),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: Container(
-                          width: double.infinity,
-                          color: Colors.transparent,
-                          child: OutlinedButton(
-                            onPressed: () {},
-                            child: Text(AppLocalizations.of(context)!
-                                .pageLoginButtonApple),
-                          ),
-                        ),
-                      ),
-                    ],
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                        return AppLocalizations.of(context)!
+                            .pageLoginUsernameError;
+                      }
+                      return null;
+                    },
                   ),
                 ),
-              ),
-            );
-          });
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: Theme.of(context).iconTheme.color,
+                      ),
+                      border: const OutlineInputBorder(),
+                      label: Text(AppLocalizations.of(context)!
+                          .pageLoginPassword),
+                      hintText: AppLocalizations.of(context)!
+                          .pageLoginPasswordHelp,
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      if (value.length < 7) {
+                        return AppLocalizations.of(context)!
+                            .pageLoginPasswordError;
+                      }
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 70),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.transparent,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        signIn(context);
+                      },
+                      child: Text(
+                          AppLocalizations.of(context)!.pageLoginButtonGoogle),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: SizedBox(
+                    width: 350,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        var databaseHelper = await DatabaseHelper();
+                        var opendb = await databaseHelper.openUserBox();
+                        var openBox = await databaseHelper.getUserBox();
+                        print(_formKey.currentState);
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          var user = await User(
+                              email: emailController.text,
+                              password: passwordController.text);
+                          var userExistsString = await databaseHelper
+                              .userExists(box: openBox, user: user);
+                          if (userExistsString != "no such user") {
+                            // ignore: use_build_context_synchronously
+                            Navigator.of(context).pushNamed(HomePage.tag);
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  BuildPopupDialog(),
+                            );
+                            // Navigator.of(context).pushNamed(NoSuchUser.tag);
+                          }
+                          print(userExistsString);
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          // ignore: use_build_context_synchronously
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 70),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.transparent,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        signIn(context);
+                      },
+                      child: Text(AppLocalizations.of(context)!
+                          .pageLoginButtonGoogle),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.transparent,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text(AppLocalizations.of(context)!
+                          .pageLoginButtonApple),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }      
+  );
 }
