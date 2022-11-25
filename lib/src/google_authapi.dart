c878a80970311934f52cfeaf4fe9b63bb735f0d6
SharovIvanY <SharovIvanY@gmail.com>
2022-11-25 19:28:45 +0300

    fix

--- lib/src/google_authapi.dart
+++ lib/src/google_authapi.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:googleapis_auth/googleapis_auth.dart';
@@ -8,19 +10,42 @@ import '../pages/home_page.dart';
 Future<AccessCredentials> GoogleAuthapi(BuildContext context) async {
   final client = http.Client();
   String url_a = '';
-  try {
-    return await obtainAccessCredentialsViaUserConsent(
-      ClientId(
-          '551314006766-4rfg36nct9if6o3hs326q3qa3qlm8kfo.apps.googleusercontent.com',
-          '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'),
-      ['email'],
-      client,
-      _prompt,
-    );
-  } finally {
-    print("HAHAH");
-    Navigator.of(context).pushNamed(HomePage.tag);
-  }
+  var a = await obtainAccessCredentialsViaUserConsent(
+    ClientId(
+        '551314006766-4rfg36nct9if6o3hs326q3qa3qlm8kfo.apps.googleusercontent.com',
+        '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'),
+    ['email'],
+    client,
+    _prompt,
+  );
+  print(a.accessToken.toJson());
+  print(a.idToken);
+  print(a.refreshToken);
+  print(a.scopes);
+  // Future<Response> post(
+  // Uri url, {
+  // Map<String, String>? headers,
+  // Object? body,
+  // Encoding? encoding,
+  var g = http.post(
+    Uri.parse('https://www.googleapis.com/auth/userinfo.email'),headers: Map('accept': 'application/json' , ),
+  );
+  return a;
+}
+  // try {
+  //   return await obtainAccessCredentialsViaUserConsent(
+  //     ClientId(
+  //         '551314006766-4rfg36nct9if6o3hs326q3qa3qlm8kfo.apps.googleusercontent.com',
+  //         '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'),
+  //     ['email'],
+  //     client,
+  //     _prompt,
+  //   );
+  // } finally {
+  //   print(client);
+  //   print("HAHAH");
+  //   Navigator.of(context).pushNamed(HomePage.tag);
+  // }
 }
 
 void _prompt(String url) {
